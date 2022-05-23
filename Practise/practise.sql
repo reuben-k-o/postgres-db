6e9e01019f19a10a7f8f4b6d2737b426ffde0f7b
reuben-k-o <rubenkhaemba@gmail.com>
2022-05-23 20:59:56 +0300

    sorting data using order by

--- Practise/practise.sql
+++ Practise/practise.sql
@@ -3,6 +3,9 @@
 SELECT DISTINCT titles FROM Employees
 
 
+-- Counting unique birth dates
+SELECT COUNT(DISTINCT birth_date) from employees
+
 /*
 * DB: World
 * Table: country
@@ -13,3 +16,12 @@ SELECT DISTINCT titles FROM Employees
 SELECT DISTINCT lifeexpectancy FROM country
 WHERE lifeexpectancy IS NOT NULL
 ORDER BY lifeexpectancy;
+
+-- sorting by name
+Select first_name, last_name from employees order By first_name, last_name desc
+
+-- sorting by age
+select age(birth_date) from employees order by age
+
+-- sort by name starting with 'k' by hire_date
+SELECT * FROM employees where first_name like 'K%' Order By hire_date
