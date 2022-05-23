f433042906fea9ef13514af894717e14a0ea403e
reuben-k-o <rubenkhaemba@gmail.com>
2022-05-23 20:40:31 +0300

    distinct operator

---
+++ Practise/practise.sql
@@ -0,0 +1,15 @@
+-- Distinct
+
+SELECT DISTINCT titles FROM Employees
+
+
+/*
+* DB: World
+* Table: country
+* Question: Can I get a list of distinct life expectancy ages
+* Make sure there are no nulls
+*/
+
+SELECT DISTINCT lifeexpectancy FROM country
+WHERE lifeexpectancy IS NOT NULL
+ORDER BY lifeexpectancy;
