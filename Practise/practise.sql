f16bba577d5b69140e913ba0f8af8db0fb127da4
reuben-k-o <rubenkhaemba@gmail.com>
2022-05-24 09:48:10 +0300

    multi table select

--- Practise/practise.sql
+++ Practise/practise.sql
@@ -25,3 +25,10 @@ select age(birth_date) from employees order by age
 
 -- sort by name starting with 'k' by hire_date
 SELECT * FROM employees where first_name like 'K%' Order By hire_date
+--------------------------------------
+-- MULTI TABLE SELECT --
+SELECT * from employees
+
+SELECT a.emp_no, concat(a.first_name, a.last_name) as "Name", b.salary From employees as a, salaries as b where a.emp_no = b.emp_no
+
+select a.emp_no, concat(a.first_name, a.last_name) as "Name", b.salary From employees as a inner join salaries as b on a.emp_no = b.emp_no order by a.emp_no
