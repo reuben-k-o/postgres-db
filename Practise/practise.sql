64ba01be9b2152a796255d627c61cc9d4ccbf713
reuben-k-o <rubenkhaemba@gmail.com>
2022-05-27 06:01:47 +0300

    conditionls

--- Practise/practise.sql
+++ Practise/practise.sql
@@ -287,3 +287,25 @@ FROM regions AS r
 JOIN departments AS d ON r.code = d.region 
 JOIN towns AS t ON d.code = t.department
 ORDER BY r.id;
+
+-------------------------------------
+--CONDITIONALS--
+
+
+/**
+* Database: Store
+* Table: products
+* Create a case statement that's named "price class" where if a product is over 20 dollars you show 'expensive'
+* if it's between 10 and 20 you show 'average' 
+* and of is lower than or equal to 10 you show 'cheap'
+
+*/
+
+SELECT actor, price, CASE 
+        WHEN price > 20 THEN 'Expensive'
+        WHEN price > 10 THEN 'Average'
+        ELSE 'Cheap'
+    END AS "price class"
+FROM products
+
+-------------------------------------
