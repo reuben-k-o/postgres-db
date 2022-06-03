bfa4a4b2ec439d3040e5875db5585e09e83aab5d
reuben-k-o <rubenkhaemba@gmail.com>
2022-06-03 15:14:36 +0300

    created suspected_rides view

--- Practise/practise.sql
+++ Practise/practise.sql
@@ -581,5 +581,6 @@ CREATE VIEW suspected_rides AS
 SELECT * FROM vehicle_location_histories AS vlh 
 WHERE city = "new york" AND 
 lat BETWEEN -74.997 AND -74.9968 AND 
-long BETWEEN 40.5 AND 40.6
-vlh.timestamp::DATE = '2020-06-23'::DATE;
+long BETWEEN 40.5 AND 40.6 AND
+vlh.timestamp::date = '2020-06-23'::date
+ORDER BY long;
